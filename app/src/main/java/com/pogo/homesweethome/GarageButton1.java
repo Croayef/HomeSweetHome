e73153d2af57a97bf78eb826f81a271d73c3f4a4
menf <menfis666@gmail.com>
2017-01-10 15:37:51 +0100

    ogrodek

--- app/src/main/java/com/pogo/homesweethome/GarageButton1.java
+++ app/src/main/java/com/pogo/homesweethome/GarageButton1.java
@@ -3,18 +3,25 @@ package com.pogo.homesweethome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class GarageButton1 extends AppCompatActivity {
 ImageView image;
+    RadioButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garage_button1);
         Switch onOffSwitch = (Switch)  findViewById(R.id.switch2);
         image = (ImageView) findViewById(R.id.imageView3) ;
+        button = (RadioButton) findViewById(R.id.radioButton);
+        button.setEnabled(false);
+
         onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
@@ -23,9 +30,20 @@ ImageView image;
                 if (isChecked==true)
                 {
                     image.setImageResource(R.drawable.garage_open);
+                    button.setEnabled(true);
+                    button.setChecked(false);
                 }
-                else if (isChecked==false)image.setImageResource(R.drawable.garage_closed);
+                else if (isChecked==false){
+                    image.setImageResource(R.drawable.garage_closed);
+                    button.setEnabled(false);
+                    button.setChecked(false);
+                }
             }
 
         });    }
+    public void Radioclick(View v){
+        Toast.makeText(this, "Auto doors closure turned on",
+                Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/com/pogo/homesweethome/GardenButton4.java
+++ app/src/main/java/com/pogo/homesweethome/GardenButton4.java
@@ -2,12 +2,36 @@ package com.pogo.homesweethome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class GardenButton4 extends AppCompatActivity {
-
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden_button4);
-    }
+        Switch onOffSwitch = (Switch)  findViewById(R.id.switch3);
+        image = (ImageView) findViewById(R.id.imageView4) ;
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.v("Switch State=", ""+isChecked);
+                if (isChecked==true)
+                {
+                    image.setImageResource(R.drawable.sprinkler_on);
+                }
+                else if (isChecked==false){
+                    image.setImageResource(R.drawable.sprinkler_off);
+                }
+            }
+
+        });    }
+
+
 }
