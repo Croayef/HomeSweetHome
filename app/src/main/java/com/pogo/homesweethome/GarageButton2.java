ce164f305877ed52d16326a63f18d61d38d12f27
menf <menfis666@gmail.com>
2017-01-10 14:04:07 +0100

    zarowa

--- app/src/main/java/com/pogo/homesweethome/GarageButton2.java
+++ app/src/main/java/com/pogo/homesweethome/GarageButton2.java
@@ -2,12 +2,30 @@ package com.pogo.homesweethome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class GarageButton2 extends AppCompatActivity {
-
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garage_button2);
-    }
+        Switch onOffSwitch = (Switch)  findViewById(R.id.switch1);
+        image = (ImageView) findViewById(R.id.imageView2) ;
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.v("Switch State=", ""+isChecked);
+                if (isChecked==true)
+                {
+                    image.setImageResource(R.drawable.light_on);
+                }
+                else if (isChecked==false)image.setImageResource(R.drawable.light_off);
+            }
+
+        });    }
 }
